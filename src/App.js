ccb004ecc7840449c48b69408b15fda5e691a452
CristoferAraya <chirss17@gmail.com>
2023-03-25 01:05:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+import MyContext from "./my_context";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./views/Home";
+import Favoritos from "./views/Favoritos";
+
+export default function App() {
+  const endpoint = "/fotos.json";
+  
+  const [fotos, setFotos] = useState([]);
+  const estadoCompartido = {fotos, setFotos};
+  
+  const fotosApi = async () => {
+    const responseFotos = await fetch(endpoint);
+    const responseFotosJSON = await responseFotos.json();
+    setFotos(responseFotosJSON.photos)
+  }
+
+  useEffect(() => {
+    fotosApi()
+  }, [])
+
+  return (
+    <div className="App">
+      <MyContext.Provider value={estadoCompartido}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContext.Provider>      
+    </div>
+  );
+}
